c90ada168b1a4b8e8433f119f6dec8b8afe26219
dhdiagram4011 <dhdiagram@gmail.com>
2020-09-08 22:49:00 +0900

    slack notification alert test

--- response_check.py
+++ response_check.py
@@ -1,7 +1,6 @@
 import requests
 import json
 
-
 ## slack url : https://app.slack.com/client/T01A7E44RNX/C01A7E451KM
 ## app id : A01A45QN893
 ## client id : 1347480161779.1344194756309
@@ -14,40 +13,39 @@ def status_check():
 
     FARGATE_HOST = 'http://13.125.124.194:8000'
 
-    SVC_PATH = [
-    '/admin', 
-    '/auth/register', 
-    '/auth/login',
-    '/auth/logout', 
-    '/auth/myinfo',
-    '/auth/edit',
-    '/auth/unregister',
-    '/reservation/revstart',
-    '/reservation/course_search',    
-    '/reservation/date_search',
-    '/reservation/date_search_result',
-    '/reservation/ticketing_list',
-    '/revapi/seatmodify',
-    '/revapi/schedulemodify',
-    '/revapi/schedule_adding',
-    '/revapi/register',
-    '/revapi/seat',
-    ] 
-  
+    SVC_PATH = (
+    "/admin", 
+    "/auth/register", 
+    "/auth/login",
+    "/auth/logout", 
+    "/auth/myinfo",
+    "/auth/edit",
+    "/auth/unregister",
+    "/reservation/revstart",
+    "/reservation/course_search",    
+    "/reservation/date_search",
+    "/reservation/date_search_result",
+    "/reservation/ticketing_list",
+    "/revapi/seatmodify",
+    "/revapi/schedulemodify",
+    "/revapi/schedule_adding",
+    "/revapi/register",
+    "/revapi/seat",
+    )
+
+
+
     for i in SVC_PATH:
+
         URL_ALL = FARGATE_HOST + str(i.split(',')).replace('[','').replace(']','').replace('\'','')
+        #URL_ALL = FARGATE_HOST + str(i)
         response = requests.get(URL_ALL)
-        health_check = response.status_code ,'+', URL_ALL.split(',')
-        jsonData = json.dumps(health_check)
-        #print (jsonData)
-        res_json = json.dumps(jsonData)
-    
-        WEB_HOOK_URL = ''
+        health_check = response.status_code ,':', URL_ALL.split(',')
+        print(health_check)
+        
+        WEB_HOOK_URL = 'https://hooks.slack.com/services/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'
         headers = {'Content-type':'application/json'}
-        print(res_json)
-        requests.post(WEB_HOOK_URL, headers=headers, data=res_json)
-        #requests.post(WEB_HOOK_URL, headers=headers, data='{"text":"alert!!!!!"}')
-
-
+        requests.post(WEB_HOOK_URL, headers=headers, data=json.dumps(health_check))
+        #requests.post(WEB_HOOK_URL, headers=headers, data='{"text":"FFF!"}')
 
 status_check()
