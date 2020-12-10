1b0d3924d719a93b87b20087bcb5de7e2a6c1dc5
dhdiagramdev4011 <dhdiagramdev4011@gmail.com>
2020-12-10 20:10:21 +0900

    twillo msg service test

---
+++ pushEngine/twillio.py
@@ -0,0 +1,36 @@
+# # Download the helper library from https://www.twilio.com/docs/python/install
+# import os
+# from twilio.rest import Client
+
+
+# account_sid = os.environ['TWILIO_ACCOUNT_SID']
+# auth_token = os.environ['TWILIO_AUTH_TOKEN']
+# client = Client(account_sid, auth_token)
+
+# message = client.messages \
+#     .create(
+#          messaging_service_sid='MG9752274e9e519418a7406176694466fa',
+#          body='body',
+#          to='+15558675310'
+#      )
+
+# print(message.sid)
+
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+                              body='This will be the body of the new message!',
+                              from_='+16468460142',
+                              to='+8201021764011'
+                          )
+
+print(message.sid)
