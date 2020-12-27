918769418b7f21f47dfa2b82164ec74fcf12fba8
dhdiagram4011 <dhdiagram@gmail.com>
2020-12-27 16:19:17 +0900

    application_start.sh modify

--- code_deploy/application_start.sh
+++ code_deploy/application_start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 cd /home/ec2-user/app
+sudo yum install python3* -y
+sudo pip3 install Django==2.1.5
+sudo pip3 install pylint
+sudo pip3 install djangorestframework
+sudo pip3 install request
 python3 /home/ec2-user/app/manage.py makemigrations
 python3 /home/ec2-user/app/manage.py migrate
-python3 /home/ec2-user/app/manage.py runserver 0:8000 &
+nohup python3 /home/ec2-user/app/manage.py runserver 0:8000 &
