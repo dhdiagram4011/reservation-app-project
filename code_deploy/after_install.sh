40d83889ecfef80f04e5cf507592b16d0cbc58b3
dhdiagram4011 <dhdiagram@gmail.com>
2020-12-31 14:10:52 +0900

    after_install.sh start_application.sh modify

--- code_deploy/after_install.sh
+++ code_deploy/after_install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 sudo yum update -y
+sudo rm -rf /home/ec2-user/* 
 mkdir -p /home/ec2-user/apps
-yum remove python3* -y
-yum install python3* -y
+sudo yum remove python3* -y
+sudo yum install python3* -y
 sudo pip3 install djangorestframework
 sudo pip3 install Django==2.1.5
 sudo pip3 install pylint
 sudo pip3 install requests
 sudo pip3 install twilio
-
-
