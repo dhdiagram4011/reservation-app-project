f2483dc8213533d16e962dfeda47ddab424a0e5c
dhdiagram4011 <dhdiagram@gmail.com>
2020-12-31 14:54:13 +0900

    application_start.sh modify

--- code_deploy/application_start.sh
+++ code_deploy/application_start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-
-python3 /home/ec2-user/apps/manage.py makemigrations
-python3 /home/ec2-user/apps/manage.py migrate
+sudo python3 /home/ec2-user/apps/manage.py makemigrations
+sudo python3 /home/ec2-user/apps/manage.py migrate
+sudo python3 /home/ec2-user/apps/manage.py runserver 0:8000 &
 
