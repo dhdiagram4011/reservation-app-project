df780026a7c13b8a46913c3f8e5090000c6586bb
dhdiagram4011 <dhdiagram@gmail.com>
2020-12-31 13:35:18 +0900

    start_application.sh modify

--- code_deploy/application_start.sh
+++ code_deploy/application_start.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-touch aaa.txt
-# sudo rm -rf /home/ec2-user/app/*
-# sudo rm -rf /home/ec2-user/app/.*
-# sudo yum update -y
-# mkdir -p /home/ec2-user/app
-# cd /home/ec2-user/app
-# sudo yum remove python3* -y
-# sudo yum install python3* -y
-# pip3 install Django==2.1.5
-# pip3 install pylint
-# pip3 install djangorestframework
-# pip3 install requests
-# pip3 install twilio
+sudo rm -rf /home/ec2-user/app/*
+sudo rm -rf /home/ec2-user/app/.*
+sudo yum update -y
+mkdir -p /home/ec2-user/app
+cd /home/ec2-user/app
+sudo yum remove python3* -y
+sudo yum install python3* -y
+pip3 install djangorestframework
+pip3 install Django==2.1.5
+pip3 install pylint
+pip3 install requests
+pip3 install twilio
+python3 /home/ec2-user/app/manage.py makemigrations
+python3 /home/ec2-user/app/manage.py migrate
