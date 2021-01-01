89e6512f657ffd06ea4ffd6e85f2cd9e0583174c
dhdiagram4011 <dhdiagram@gmail.com>
2021-01-01 16:46:27 +0900

    twilio.env file add - source

--- code_deploy/application_start.sh
+++ code_deploy/application_start.sh
@@ -2,6 +2,7 @@
 
 sudo python3 /home/ec2-user/apps/manage.py makemigrations
 sudo python3 /home/ec2-user/apps/manage.py migrate
+source /home/ec2-user/apps/twilio.env
 ##sudo python3 /home/ec2-user/apps/manage.py runserver 0:8000 & \
 
 
