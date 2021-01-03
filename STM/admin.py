ad965b7050daf727040fe93bf5350692b6dd8a8a
dhdiagram4011 <dhdiagram@gmail.com>
2021-01-03 13:55:38 +0900

    점포별 관리 앱(cafe_manage) 생성 및 업로드

--- STM/admin.py
+++ STM/admin.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from .models import *
 
 
+# 앱 사용을 위한 점포회원 가입
 class STMAdmin(admin.ModelAdmin):
     list_display = ['id','business_id','MG_number','Store_name','Store_phone','Store_address_01','Store_address_02','Representative','store_number','bln','stamp_design']
 
-
 admin.site.register(STM,STMAdmin)
-
-
 
