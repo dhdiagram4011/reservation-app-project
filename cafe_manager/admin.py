ab383c6321c615e5c82fab3a3294e9ecf89f805c
dhdiagram4011 <dhdiagram@gmail.com>
2021-01-09 17:55:03 +0900

    cafe_manger app db 테이블 생성 오류 확인중

--- cafe_manager/admin.py
+++ cafe_manager/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import StoreMember
 
-# Register your models here.
+class StoreMemberAdmin(admin.ModelAdmin):
+    list_display = ['id','customer_name','customer_phone','customer_address','customer_birth','stamp_number','customer_create']
+
+
+admin.site.register(StoreMember, StoreMemberAdmin)
--- cafe_manager/models.py
+++ cafe_manager/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
-#cafe_manager
 """
 개인 점포별 관리 페이지
 dhdiagram.net/${business_id} 형식으로 생성
 """
+
+#카페별 고객 정보 수집을 위한 테이블
+class StoreMember(models.Model):
+    customer_name = models.CharField(max_length=4)  #고객이름
+    customer_email = models.EmailField(max_length=100)  #고객 이메일 주소
+    customer_phone = models.CharField(max_length=20) #고객 전화번호
+    customer_address = models.CharField(max_length=100) #고객 주소
+    customer_birth = models.DateField(auto_now_add=True) #고객 생일
+    stamp_number = models.IntegerField(max_length=2) #고객 스탬프 적립 갯수
+    customer_create = models.DateTimeField(auto_now_add=False) #가입일
+    
